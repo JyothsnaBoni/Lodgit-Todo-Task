db1c125afeb938140541f2bb21f10be944083135
JyothsnaBoni <boni.jyothsna@gmail.com>
2019-05-25 03:54:47 -0400

    Update database.js

--- TodoServer/database.js
+++ TodoServer/database.js
@@ -8,7 +8,7 @@ let sqlite3 = require('sqlite3').verbose();
 /* Load database file (Creates file if not exists) */
 let db = new sqlite3.Database('./sqlite.db');
 
-/* Init car and driver tables if they don't exist */
+/* Init todo and category tables if they don't exist */
 let init = function () {
     db.run(`CREATE TABLE if not exists todo(subject TEXT NOT NULL,status TEXT, created TEXT, modified TEXT, category TEXT,PRIMARY KEY ( subject, category))`);
       
